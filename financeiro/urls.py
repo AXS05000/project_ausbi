df180ef4c417010be745cce9959337556f62d8dc
AXS05000 <axssxs00@gmail.com>
2023-07-29 00:20:36 -0300

    V - 29-07-2023 - 00:20

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -1,16 +1,20 @@
 # urls.py
 from django.urls import path
 
-from .views import (TransacaoCreate, TransacaoDelete, TransacaoUpdate,
-                    TransacoesMes, VisaoGeral)
+from .views import (DashboardView, TablesView, TransacaoCreate,
+                    TransacaoDelete, TransacaoUpdate, TransacoesMes,
+                    VisaoGeral, WalletView)
 
 # urls.py
 
 urlpatterns = [
     path('visao/', VisaoGeral.as_view(), name='visao-geral'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('wallet/', WalletView.as_view(), name='wallet'),
+    path('tables/', TablesView.as_view(), name='tables'),
     path('transacao/nova/', TransacaoCreate.as_view(), name='transacao-nova'),
     path('transacao/<int:pk>/editar/', TransacaoUpdate.as_view(), name='transacao-editar'),
     path('transacao/<int:pk>/deletar/', TransacaoDelete.as_view(), name='transacao-deletar'),
-    path('transacao/mes/<int:mes>/', TransacoesMes.as_view(), name='transacoes-mes'),  # nova linha
+    path('transacao/mes/<int:mes>/', TransacoesMes.as_view(), name='transacoes-mes'),
 ]
 
